a3b43fa58b8fb8293995666be2462be76ff98774
Victor Fontenele <victorvitoria11@hotmail.com>
2020-05-18 19:41:03 -0300

    feat: cors

--- modulo01/src/app.ts
+++ modulo01/src/app.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 import express from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 
 import routes from './routes';
 
@@ -11,6 +12,7 @@ import HandleExceptionMiddleware from './middlewares/HandleException';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
