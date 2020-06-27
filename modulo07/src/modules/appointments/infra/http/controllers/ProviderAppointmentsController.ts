fdba1f712697dccb9c3871b651153e3316a1c2a7
Victor Fontenele <victorvitoria11@hotmail.com>
2020-06-26 21:30:36 -0300

    fix(appointments): requests on controllers

--- modulo07/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ modulo07/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -3,13 +3,13 @@ import { container } from 'tsyringe';
 
 import ListProviderAppointmentsService from '@modules/appointments/services/ListProviderAppointmentsService';
 
-export default class ProviderDayAvailabilityController {
+export default class ProviderAppointmentsController {
     public async index(
         request: Request,
         response: Response,
     ): Promise<Response> {
         const provider_id = request.user.id;
-        const { day, month, year } = request.body;
+        const { day, month, year } = request.query;
 
         const listProviderAppointments = container.resolve(
             ListProviderAppointmentsService,
@@ -17,9 +17,9 @@ export default class ProviderDayAvailabilityController {
 
         const appointments = await listProviderAppointments.execute({
             provider_id,
-            day,
-            month,
-            year,
+            day: Number(day),
+            month: Number(month),
+            year: Number(year),
         });
 
         return response.json(appointments);
